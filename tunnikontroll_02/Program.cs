cb43bfb323c0198767d0d08d92e2d6a8b846b111
Mikk <32930204+mikkmuru1@users.noreply.github.com>
2018-01-15 11:55:14 +0200

    asd

---
+++ tunnikontroll_02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tunnikontroll_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programm mis lubab sisestada N+1 hulga erinevaid sõnu, kuni sisestatakse [-1]");
+            Console.WriteLine("");
+
+
+            // Asks user input until [-1] is entered.
+            while (true)
+            {
+                Console.Write("Sisesta sõna: ");
+                string userInput = Console.ReadLine();
+                string[] allInput = new string[] {userInput};
+                
+                if( userInput == "-1")
+                {
+                    // Displays all the input 
+                    Console.WriteLine("");
+                    Console.WriteLine("Sisestasid sõnad:{0} , ", allInput);
+
+                    // Chooses a random word and displays it
+                    Console.WriteLine("");
+                    Console.WriteLine("Suvaline sõna: ");
+
+                    break;    
+                    
+                }
+
+                
+            }
+
+            Console.WriteLine("");                      
+            Console.WriteLine("Press any key to finish.");
+            Console.ReadKey();
+            
+        }
+    }
+}
