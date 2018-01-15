2f3ce0af301152775258eea82e6121b3f019e110
Mikk <32930204+mikkmuru1@users.noreply.github.com>
2018-01-15 12:49:45 +0200

    Add files via upload

---
+++ tunnikontroll_02/tunnikontroll_02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tunnikontroll_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programm mis lubab sisestada N+1 hulga erinevaid sõnu, kuni sisestatakse [-1]");
+            Console.WriteLine("");
+            List<string> allInput = new List<string>();
+            
+
+            // Asks user input until [-1] is entered.
+            while (true)
+            {
+                Console.Write("Sisesta sõna: ");
+                string userInput = Console.ReadLine();
+               
+                
+                if (userInput != "-1")
+                {
+                    allInput.Add(userInput);
+                }
+                else
+                    break;
+                
+            }
+
+            Console.WriteLine("Sisestasid sõnad:{0} ", allInput);
+
+
+            Random rnd = new Random();
+            int inputx = rnd.Next(allInput.Count);
+            Console.WriteLine("Suvaline sõna on: " + allInput[inputx]);
+
+            Console.WriteLine("");                      
+            Console.WriteLine("Press any key to finish.");
+            Console.ReadKey();
+            
+        }
+    }
+}
